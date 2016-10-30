ef8b4216e5d024178179297e4e48466da23778c6
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-10-29 20:00:29 -0400

    Added the gzcompression functions trick

--- tipsandtricks.php
+++ tipsandtricks.php
@@ -173,6 +173,45 @@ Array
     [3] => apple
 )
 */
+
+//trick8
+echo '<h3>8:  Using Compression:</h3>';
+$string =
+"Lorem ipsum dolor sit amet, consectetur
+adipiscing elit. Nunc ut elit id mi ultricies
+adipiscing. Nulla facilisi. Praesent pulvinar,
+sapien vel feugiat vestibulum, nulla dui pretium orci,
+non ultricies elit lacus quis ante. Lorem ipsum dolor
+sit amet, consectetur adipiscing elit. Aliquam
+pretium ullamcorper urna quis iaculis. Etiam ac massa
+sed turpis tempor luctus. Curabitur sed nibh eu elit
+mollis congue. Praesent ipsum diam, consectetur vitae
+ornare a, aliquam a nunc. In id magna pellentesque
+tellus posuere adipiscing. Sed non mi metus, at lacinia
+augue. Sed magna nisi, ornare in mollis in, mollis
+sed nunc. Etiam at justo in leo congue mollis.
+Nullam in neque eget metus hendrerit scelerisque
+eu non enim. Ut malesuada lacus eu nulla bibendum
+id euismod urna sodales. ";
+echo 'The gzcompress function enables us to compress long strings so that they take up less space.<br>';
+echo 'Here is an example string: <br><br>';
+echo "$string";
+
+$compressed = gzcompress($string);
+ 
+echo "<br><br>The original size of the string was : ". strlen($string)."<br>";
+/* prints
+Original size: 779
+*/
+echo "The compressed size of the string is: ". strlen($compressed)."<br>";
+/* prints
+Compressed size: 408
+*/
+echo '<br>The gzuncompress function uncompresses compressed strings.';
+// getting it back
+$original = gzuncompress($compressed);
+
+
 ?>
 </body>
 </html>
